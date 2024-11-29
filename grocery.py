f826aba4b3eae076ab7fd891ca74bd331df5c755
David <deivid.prz3412@gmail.com>
2024-11-28 21:24:19 -0400

    last version optimized

--- grocery.py
+++ grocery.py
@@ -1,24 +1,22 @@
 def main():
 
     grocery_list = []
-    item_count = []
+    item_count = {}
     while True:
         try:
             grocery_list.append(input().strip().upper())
         except EOFError:
 
             grocery_list.sort()
-            for i in grocery_list:
+            for grocery in grocery_list:
+                count = grocery_list.count(grocery)
 
-                count = grocery_list.count(i)
-
-                if not i in [key["item"] for key in item_count]:
-                    item_count.append({"item": i, "count": count})
-
-            grocery_items = sorted(set(grocery_list))
-
-            for (i, item) in enumerate(grocery_items):
-                print(f"{item_count[i]["count"]}. {item}")
+                if not item_count.get(grocery):
+                    item_count[grocery] = count
             
+            for item in item_count:
+                print(f"{item_count[item]} {item}")
+
+
             break
 main()
