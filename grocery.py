7dbe16da42bf56af30878f997e5ae596245b101f
David <deivid.prz3412@gmail.com>
2024-11-28 21:14:08 -0400

    sorted set

--- grocery.py
+++ grocery.py
@@ -4,19 +4,21 @@ def main():
     item_count = []
     while True:
         try:
-            grocery_list.append(input().upper())
+            grocery_list.append(input().strip().upper())
         except EOFError:
 
+            grocery_list.sort()
             for i in grocery_list:
 
                 count = grocery_list.count(i)
-                
-                if not i in [subkey for key in item_count for subkey in key.values()]:
+
+                if not i in [key["item"] for key in item_count]:
                     item_count.append({"item": i, "count": count})
 
-            grocery_items = set(grocery_list)
-            
+            grocery_items = sorted(set(grocery_list))
+
             for (i, item) in enumerate(grocery_items):
                 print(f"{item_count[i]["count"]}. {item}")
-
+            
+            break
 main()
