91081a7dca1e701fec8939801e7496bf57021422
David <deivid.prz3412@gmail.com>
2024-11-29 20:43:39 -0400

    guess game

---
+++ guessGame.py
@@ -0,0 +1,31 @@
+import random
+
+def main():
+
+    while True:
+        try:
+            user_number = int(input('Level: '))
+        except ValueError:
+            pass
+        else:
+            break
+    
+    winning_number = random.randint(1, user_number)
+
+    while True:
+        try:
+            guess = int(input("Guess: "))
+
+            if guess > winning_number:
+                print("Too Large")
+            elif guess < winning_number:
+                print("Too Small")
+            else: 
+                print('Just Right!')
+                break
+        except ValueError:
+            pass
+
+
+if __name__ == "__main__":
+    main()
