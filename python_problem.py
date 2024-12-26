e93f555855cce0aa1953bf685d45a040b76ead8b
David <deivid.prz3412@gmail.com>
2024-12-25 20:19:31 -0400

    update

--- python_problem.py
+++ python_problem.py
@@ -1,32 +1,50 @@
 def main():
-    shoes = [ {"type": "I", "size": 38}, {"type": "R", "size": 38}, {"type": "R", "size": 42}, {"type": "I", "size": 41}, {"type": "I", "size": 42} ]
 
-    print(organize(shoes))
+    print(
+        inBox([
+            "###",
+            "#*#",
+            "###"
+        ])
+    ) # ➞ True
 
-
-def organize(shoes):
-    organized = []
-    paired = []
-
-    for i in range(len(shoes)):
-
-        for j in range(i + 1, len(shoes)):
-
-            if shoes[i]["size"] == shoes[j]["size"]:
-
-                first_index = i
-                second_index = j
-
-                if (
-                    (shoes[i]["type"] == "I" and shoes[j]["type"] == "R")
-                    or (shoes[i]["type"] == "R" and shoes[j]["type"] == "I")
-                ) and (not (first_index in paired) and not (second_index in paired)):
-
-                    organized.append(shoes[i]["size"])
-                    paired.extend([first_index, second_index])
-                    break
+    print(inBox([
+        "####",
+        "#* #",
+        "#  #",
+        "####"
+    ])) # ➞ True
     
-    return organized
+    print(inBox([
+        "#####",
+        "#   #",
+        "#  #*",
+        "#####"
+    ])) # ➞ False
+
+    print(inBox([
+        "#####",
+        "#   #",
+        "#   #",
+        "#   #",
+        "#####"
+    ])) # ➞ False
+
+
+def inBox(box):
+    
+    contained = False
+    for row in box:
+        if "*" in box[0] or "*" in box[-1]:
+            return False
+
+        if (not row[0] == "*" and not row[-1] == "*") and "*" in row[1:-1]:
+            contained = True
+
+    if contained:
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
